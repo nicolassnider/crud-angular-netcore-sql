26989419fea5270a82143de7df2360e4d7753762
Nicolás Snider <nicolas.snider@soulit.io>
2021-09-09 00:10:25 -0300

    2da partebackend

---
+++ BETarjetaCredito/BETarjetaCredito/Migrations/20210908234908_v1.0.0.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BETarjetaCredito.Migrations
+{
+    public partial class v100 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TarjetaCredito",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    CardHolder = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CCN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Expire = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CVV = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TarjetaCredito", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TarjetaCredito");
+        }
+    }
+}
---
+++ BETarjetaCredito/BETarjetaCredito/Models/TarjetaCredito.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BETarjetaCredito.Models
+{
+    public class TarjetaCredito
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string CardHolder { get; set; }
+        [Required]
+        public string CCN { get; set; }
+        [Required]
+        public string Expire { get; set; }
+        [Required]
+        public string CVV {get;set;}
+
+    }
+}
